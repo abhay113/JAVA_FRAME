0a49df97d6b436619858dddc071f0664953bb4a4
ABHAY CHAVAN <75495921+abhay113@users.noreply.github.com>
2023-08-31 22:12:31 +0530

    Add files via upload

---
+++ src/main/java/com/abhay/abhay_8944.java
@@ -0,0 +1,13 @@
+package com.abhay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class abhay_8944 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(abhay_8944.class, args);
+	}
+
+}
